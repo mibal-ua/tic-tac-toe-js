6f3439f4885ca69bf8df85f8157bc998be4b002c
mibal_ua <9mohapx9@gmail.com>
2022-12-28 16:41:03 +0100

    Use imported GameTable

--- src/main/javascript/launcher.js
+++ src/main/javascript/launcher.js
@@ -1,37 +1,8 @@
 'use strict';
 
-import { Cell } from "./structures/Cell.js";
+import { GameTable } from "./structures/GameTable";
 
 import { Sign } from "./structures/Sign.js";
-
-class GameTable {
-
-    constructor() {
-        initializeCells(this);
-    }
-
-    cellIsEmpty(id) {
-        const cell = this.data[id];
-        return cell.sign === Sign.EMPTY;
-    }
-
-    setSign(id, sign) {
-        const cell = this.data[id];
-        if (cell.sign === Sign.EMPTY) {
-            cell.setSign(sign);
-        }
-    }
-
-    print() {
-        const gameTableDiv = document.getElementById("gameTable");
-        gameTableDiv.innerHTML = '';
-        const array = this.data;
-        for (const key in array) {
-            const cell = array[key];
-            gameTableDiv.appendChild(cell.html);
-        }
-    }
-}
 
 const userMove = (id, gameTable) => () => {
     if (gameTable.cellIsEmpty(id)) {
@@ -40,28 +11,6 @@ const userMove = (id, gameTable) => () => {
         // TODO computerMove
         gameTable.print();
     }
-}
-
-const initializeCells = (gameTable) => {
-    const array = [];
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            array[`${i}, ${j}`] = cellFactory(`${i}, ${j}`, gameTable);
-        }
-    }
-    gameTable.data = array;
-}
-
-function cellFactory(id, gameTable) {
-    const htmlCell = document.createElement('div');
-    htmlCell.className = 'cell';
-    htmlCell.id = id;
-    const textElement = document.createElement('p');
-    textElement.className = 'sign';
-    const sign = Sign.EMPTY
-    htmlCell.appendChild(textElement);
-    htmlCell.onclick = userMove(htmlCell.id, gameTable);
-    return new Cell(htmlCell, sign);
 }
 
 // game logic
