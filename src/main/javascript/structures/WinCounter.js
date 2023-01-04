a96d45b84188db10cfa64ea8c2dd78b3285aba42
mibal_ua <9mohapx9@gmail.com>
2023-01-04 19:19:27 +0100

    Add and realize win counter component

---
+++ src/main/javascript/structures/WinCounter.js
@@ -0,0 +1,19 @@
+const winCounter = counterFabric();
+
+function counterFabric() {
+    const score = {
+        player: 0,
+        computer: 0
+    };
+    return (message) => {
+        if (message.includes('Player')) {
+            score.player += 1;
+        } else if (message.includes('Computer')) {
+            score.computer += 1;
+        }
+        const counter = document.getElementById("counter");
+        counter.innerText = `${score.player}:${score.computer}`
+    };
+}
+
+export { winCounter };
