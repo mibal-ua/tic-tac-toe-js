5f0cfbdb967a83adaab80375be173a1a98ec37d9
mibal_ua <9mohapx9@gmail.com>
2023-01-05 11:44:58 +0100

    Implement FirstMoveToTheCenterComputerMoveStrategy

--- src/main/javascript/components/strategies/FirstMoveToTheCenterComputerMoveStrategy.js
+++ src/main/javascript/components/strategies/FirstMoveToTheCenterComputerMoveStrategy.js
@@ -1,6 +1,17 @@
-export class FirstMoveToTheCenterComputerMoveStrategy {
+'use strict';
+
+class FirstMoveToTheCenterComputerMoveStrategy {
 
     tryToMakeMove(gameTable, moveSign) {
-
+        const cellId = '1, 1';
+        if (gameTable.cellIsEmpty(cellId)) {
+            gameTable.setSign(cellId, moveSign);
+            return true;
+        } else {
+            return false;
+        }
     }
+
 }
+
+export { FirstMoveToTheCenterComputerMoveStrategy };
