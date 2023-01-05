204d8987e9de1852b8b5582ab63733a08e82770e
mibal_ua <9mohapx9@gmail.com>
2023-01-05 11:42:33 +0100

    Implement PreventUserWinComputerMoveStrategy

--- src/main/javascript/components/strategies/PreventUserWinComputerMoveStrategy.js
+++ src/main/javascript/components/strategies/PreventUserWinComputerMoveStrategy.js
@@ -1,6 +1,18 @@
-export class PreventUserWinComputerMoveStrategy {
+import { Sign } from "../../structures/Sign.js";
 
-    tryToMakeMove(gameTable, moveSign) {
+import { AbstractComputerMoveStrategy } from './AbstractComputerMoveStrategy.js'
 
+class PreventUserWinComputerMoveStrategy extends AbstractComputerMoveStrategy {
+
+    constructor() {
+        super(1);
+    }
+
+    // Override
+    getFindSign(moveSign) {
+        if (moveSign === Sign.O) return Sign.X;
+        if (moveSign === Sign.X) return Sign.O;
     }
 }
+
+export { PreventUserWinComputerMoveStrategy };
