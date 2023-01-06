09912f5c148474d7925e13d34bd432df1bf8b86d
mibal_ua <9mohapx9@gmail.com>
2023-01-06 10:50:51 +0100

    Add hover score effect

--- src/main/javascript/components/WinCounter.js
+++ src/main/javascript/components/WinCounter.js
@@ -5,18 +5,38 @@ function counterFabric() {
         player: 0,
         computer: 0
     };
+    addHoverScoreEffect(score);
     return (message) => {
         if (message.includes('Player')) {
             score.player += 1;
         } else if (message.includes('Computer')) {
             score.computer += 1;
-        } else if (message.includes('RESTART')){
+        } else if (message.includes('RESTART')) {
             score.computer = 0;
             score.player = 0;
         }
+        addHoverScoreEffect(score);
         const counter = document.getElementById("counter");
         counter.innerText = `${score.player}:${score.computer}`
     };
+}
+
+function addHoverScoreEffect(score) {
+    const counter = document.getElementById("counter");
+    counter.addEventListener("mouseover", showSigns, false);
+    counter.addEventListener("mouseout", showScore(score), false);
+}
+
+function showSigns() {
+    const counter = document.getElementById("counter");
+    counter.innerText = 'X:O';
+}
+
+function showScore(score) {
+    return () => {
+        const counter = document.getElementById("counter");
+        counter.innerText = `${score.player}:${score.computer}`
+    }
 }
 
 export { winCounter };
