f8a8a4d3cd15e5949b5db9b6502546bb3eec084e
mibal_ua <9mohapx9@gmail.com>
2023-01-06 09:28:50 +0100

    Realize static getOppositeSign method

--- src/main/javascript/structures/Sign.js
+++ src/main/javascript/structures/Sign.js
@@ -4,6 +4,12 @@ class Sign {
     static X = { text: 'X' };
     static O = { text: '0' };
     static EMPTY = { text: ' ' };
+
+    static getOppositeSign(sign) {
+        if (sign === this.X) return this.O;
+        if (sign === this.O) return this.X;
+        return this.EMPTY;
+    }
 }
 
 export { Sign };
