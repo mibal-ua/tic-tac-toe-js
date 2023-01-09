2b8dba276b93e4d44a31125cf9147b0ef549b51f
mibal_ua <9mohapx9@gmail.com>
2023-01-09 12:51:20 +0100

    Change lambda syntax to function declaration

--- src/main/javascript/structures/GameTable.js
+++ src/main/javascript/structures/GameTable.js
@@ -4,30 +4,6 @@ import { Sign } from './Sign.js';
 
 import { Cell } from './Cell.js';
 
-const initializeCells = (gameTable, onClickEvent) => {
-
-    function cellFactory(id, gameTable) {
-        const htmlCell = document.createElement('div');
-        htmlCell.className = 'cell';
-        htmlCell.id = id;
-        const textElement = document.createElement('p');
-        textElement.className = 'sign';
-        const sign = Sign.EMPTY
-        htmlCell.appendChild(textElement);
-        htmlCell.onclick = onClickEvent(htmlCell.id, gameTable);
-        return new Cell(htmlCell, sign);
-    }
-
-    const array = [];
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            const id = `${i}, ${j}`;
-            array[id] = cellFactory(id, gameTable);
-        }
-    }
-    return array;
-}
-
 class GameTable {
 
     constructor(onClickEvent) {
@@ -78,6 +54,30 @@ class GameTable {
     setLastSign(sign) {
         this.lastSign = sign;
     }
+}
+
+function initializeCells(gameTable, onClickEvent) {
+
+    function cellFactory(id, gameTable) {
+        const htmlCell = document.createElement('div');
+        htmlCell.className = 'cell';
+        htmlCell.id = id;
+        const textElement = document.createElement('p');
+        textElement.className = 'sign';
+        const sign = Sign.EMPTY
+        htmlCell.appendChild(textElement);
+        htmlCell.onclick = onClickEvent(htmlCell.id, gameTable);
+        return new Cell(htmlCell, sign);
+    }
+
+    const array = [];
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            const id = `${i}, ${j}`;
+            array[id] = cellFactory(id, gameTable);
+        }
+    }
+    return array;
 }
 
 export { GameTable };
